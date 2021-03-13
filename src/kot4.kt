bf2afb559200325bf7bc3d0e5b8f2ae0afb40259
gerardine2001 <gerardinekaba@gmail.com>
2021-03-13 14:27:31 +0300

    kot4

---
+++ src/kot4.kt
@@ -0,0 +1,26 @@
+fun main() {
+
+     word()
+     beauty()
+     sentence("gerardine",20)
+}
+
+fun word() {
+    var text = "gerardine"
+    println(text[0])
+}
+
+fun beauty(){
+    var name="gerardine"
+    println(name.toUpperCase())
+}
+fun sentence(name:String,age:Int) {
+    println("Hello,my name is $name. I am $age years old")
+}
+
+
+
+
+
+
+
